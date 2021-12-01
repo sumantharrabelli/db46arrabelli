10e0353dbb81510e55d658ecd1238ef42e8daa67
sumantharrabelli <arrabellisumanth85@gmail.com>
2021-12-01 00:40:13 -0600

    added part 5

--- routes/gucci.js
+++ routes/gucci.js
@@ -2,6 +2,17 @@ var express = require('express');
 const gucci_controllers = require('../controllers/gucci');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 ///* GET home page. */
 //router.get('/', function (req, res, next) {
 //  res.render('gucci', { title: 'Search Results for guccis' });
@@ -14,7 +25,7 @@ router.get('/detail', gucci_controllers.gucci_view_one_Page);
 /* GET create gucci page */
 router.get('/create', gucci_controllers.gucci_create_Page);
 /* GET create update page */
-router.get('/update', gucci_controllers.gucci_update_Page);
+router.get('/update', secured, gucci_controllers.gucci_update_Page);
 /* GET create delete page */
 router.get('/delete', gucci_controllers.gucci_delete_Page);
 /* GET details with id of gucci page */
