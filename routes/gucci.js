95127bb462600644ea354bc93bd19e5e0d8fa996
sumantharrabelli <arrabellisumanth85@gmail.com>
2021-12-01 00:51:30 -0600

    added part 6 and bonus

--- routes/gucci.js
+++ routes/gucci.js
@@ -23,11 +23,11 @@ router.get('/', gucci_controllers.gucci_view_all_Page);
 /* GET detail gucci page */
 router.get('/detail', gucci_controllers.gucci_view_one_Page);
 /* GET create gucci page */
-router.get('/create', gucci_controllers.gucci_create_Page);
+router.get('/create', secured, gucci_controllers.gucci_create_Page);
 /* GET create update page */
 router.get('/update', secured, gucci_controllers.gucci_update_Page);
 /* GET create delete page */
-router.get('/delete', gucci_controllers.gucci_delete_Page);
+router.get('/delete', secured, gucci_controllers.gucci_delete_Page);
 /* GET details with id of gucci page */
 router.get('/gucci/:id', gucci_controllers.gucci_detail);
 /* DELETE details with id of gucci page */
